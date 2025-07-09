e946cf9db5f21dfe2d4b8a812d66e2a43b59d55f
Adejare Faruk <nou183023868@noun.edu.ng>
2025-07-09 16:40:05 +0100

    Added Header.jsx,MainContent.jsx,Footer.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,11 +2,17 @@ import { useState } from "react";
 
 import "./App.css";
 import WelcomeMessage from "./components/WelcomeMessage";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
+      <Header />
+      <MainContent />
       <WelcomeMessage />
+      <Footer />
     </>
   );
 }
