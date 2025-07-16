bf47ea0f8dc43c6b357eb74af97bd349140fafef
Adejare Faruk <nou183023868@noun.edu.ng>
2025-07-16 16:53:57 +0100

    update

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,13 +1,23 @@
-import ProfilePage from './components/ProfilePage';
-import UserContext from './components/UserContext';
+import Footer from "../../alx-react-app/src/components/Footer";
+import Header from "../../alx-react-app/src/components/Header";
+import MainContent from "../../alx-react-app/src/components/MainContent";
+import WelcomeMessage from "../../alx-react-app/src/components/WelcomeMessage";
+import ProfilePage from "./components/ProfilePage";
+import UserContext from "./components/UserContext";
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
-    <UserContext.Provider value={userData}>
-      <ProfilePage />
-    </UserContext.Provider>
+    <>
+      <Header />
+      <MainContent />
+      <WelcomeMessage />
+      <UserContext.Provider value={userData}>
+        <ProfilePage />
+      </UserContext.Provider>
+      <Footer />
+    </>
   );
 }
 
