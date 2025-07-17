ddb9f2ce7abd1241fd1b9823f8c30015abeda794
Adejare Faruk <nou183023868@noun.edu.ng>
2025-07-17 19:44:19 +0100

    update

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,17 +1,25 @@
-// import './App.css'
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Home from "./components/Home";
-import Services from "./components/Services";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+// import Footer from './components/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import Services from './pages/Services';
+import Contact from './pages/Contact';
 
 function App() {
   return (
-    <>
-      <Home />
-      <About />
-      <Services />
-      <Contact />
-    </>
+    <BrowserRouter>
+      <Navbar />
+      <div style={{ minHeight: '80vh' }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+      {/* <Footer /> */}
+    </BrowserRouter>
   );
 }
 
