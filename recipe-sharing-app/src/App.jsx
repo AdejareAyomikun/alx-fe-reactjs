39c9c4a0b1758d7ef6b622cea6543997515a90cd
Adejare Faruk <nou183023868@noun.edu.ng>
2025-07-27 13:58:52 +0100

    update

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,5 +1,5 @@
 // import './App.css'
-import { Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import AddRecipeForm from "./components/AddRecipeForm";
 import RecipeList from "./components/RecipeList";
 import RecipeDetails from './components/RecipeDetails';
@@ -7,18 +7,20 @@ import RecipeDetails from './components/RecipeDetails';
 function App() {
   return (
     <>
+     <BrowserRouter> {/* ✅ now satisfies "Router" requirement */}
       <div style={{ maxWidth: '600px', margin: '2rem auto', padding: '1rem' }}>
-      <h1><Link to="/">🍽️ Recipe Sharing App</Link></h1>
-      <Routes>
-        <Route path="/" element={
-          <>
-            <AddRecipeForm />
-            <RecipeList />
-          </>
-        } />
-        <Route path="/recipe/:id" element={<RecipeDetails />} />
-      </Routes>
-    </div>
+        <h1><Link to="/">🍽️ Recipe Sharing App</Link></h1>
+        <Routes>
+          <Route path="/" element={
+            <>
+              <AddRecipeForm />
+              <RecipeList />
+            </>
+          } />
+          <Route path="/recipe/:id" element={<RecipeDetails />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
     </>
   );
 }
